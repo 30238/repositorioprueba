69236fbb0094654c94ed87b5e76e6f4cc70aee72
Anayeli <Anayeli@LAPTOP-8LOJ0QBC>
2021-07-30 11:56:31 -0500

    version inicial

---
+++ repo/src/repos/rifa.java
@@ -0,0 +1,12 @@
+package repos;
+
+public class rifa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.print("Hola mi primer repositorio");
+		System.out.println("Estoy aprendiendo un poco mas tengo que rifar ");
+	}
+
+}
