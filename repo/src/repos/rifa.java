ab2666a6219f2d1b5ae32991a3f375b2dbb618f4
Anayeli <Anayeli@LAPTOP-8LOJ0QBC>
2021-07-30 17:19:47 -0500

    f

--- repo/src/repos/rifa.java
+++ repo/src/repos/rifa.java
@@ -6,7 +6,8 @@ public class rifa {
 		// TODO Auto-generated method stub
  
 		System.out.print("Hola mi primer repositorio");
-		System.out.println("dato nuevso");
+		System.out.println("dato nuefvsof");
+		System.out.println("dato nuefvsof");
 	}
 
 }
